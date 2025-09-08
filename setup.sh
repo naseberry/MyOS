a70d475e38db7b697abb47ed673af7d1c7ce09bb
Peter Hunt <naseberry@users.noreply.github.com>
2025-09-08 14:48:46 +0100

    Add VPN gui

--- setup.sh
+++ setup.sh
@@ -96,7 +96,7 @@ sudo systemctl enable tlp.service
 # VPN
 ####################
 
-sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+sh <(wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh) -p nordvpn-gui
 
 
 ####################
